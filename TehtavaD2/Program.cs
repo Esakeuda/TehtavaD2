e3e11f2c6eb074ecebc59654358d332ff138460f
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-22 05:36:39 +0300

    Add project files.

---
+++ TehtavaD2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CelsiusToFahrenheit
+{
+    class Program
+    {
+        // Funktio muuntaa Celsius-asteet Fahrenheiteiksi
+        static double CelsiusToFahrenheit(double celsius)
+        {
+            // Muunnoskaava: Fahrenheit = (Celsius * 1.8) + 32
+            double fahrenheit = celsius * 1.8 + 32;
+            return fahrenheit;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä lämpötila Celsius-asteina: ");
+            double celsiusInput = Convert.ToDouble(Console.ReadLine());
+
+            // Kutsutaan muuntamisfunktiota
+            double fahrenheitOutput = CelsiusToFahrenheit(celsiusInput);
+
+            // Tulostetaan muunnettu lämpötila
+            Console.WriteLine($"{celsiusInput} Celsius-astetta on {fahrenheitOutput:F2} Fahrenheit-astetta.");
+        }
+    }
+}
